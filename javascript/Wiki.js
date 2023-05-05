f62089e4cf7f53b2175b9547ee7d5f8405623de2
Meme1079 <101881784+Meme1079@users.noreply.github.com>
2023-05-05 04:06:58 +0000

    Epic SideBar Added

--- javascript/Wiki.js
+++ javascript/Wiki.js
@@ -7,7 +7,7 @@ function getNewLineCode(preInd = 0) {
           results = Number(dumbyLines) + 1
      }
      for (let limit = 1; limit <= results; limit++) {
-          let insertElement = `<span style="display: block;">${limit.toString()}</span>`
+          let insertElement = `<span>${limit.toString()}</span>`
           shortCutAll('.code-newlines')[preInd].innerHTML += insertElement
      }
 }
@@ -22,7 +22,11 @@ function importTableDatas(jsonData, tag, styleVal = ['']) {
                <td style="${styleVal[1]}">${jsonData[datas][2]}</td>
           </tr>`
      }
-     return shortCut(tag).innerHTML = tableDataResult
+     try {
+          return shortCut(tag).innerHTML = tableDataResult
+     } catch (error) {
+          return null
+     }
 }
 
 for (let preLength = 0; preLength < shortCutAll('pre').length; preLength++) {
@@ -44,3 +48,65 @@ switch (shortCutAll('title')[0].textContent.split(': ')[1]) {
      default:
           break;
 }
+
+/* The SideBar */
+
+// You: How many "For Loops" you want?
+// Meme1079: yes
+
+for (let headIncre = 0; headIncre <= 4; headIncre++) {
+     let HeadNum = `#wiki h${headIncre.toString()}`
+     for (let headerClass = 0; headerClass < shortCutAll(HeadNum).length; headerClass++) {
+          shortCutAll(HeadNum)[headerClass].setAttribute('class', `header header${headIncre.toString()}`)
+     }
+}
+
+const HeaderMainPath = '#sidebar-headerlists ul'
+for (let i = 0; i < shortCutAll('#wiki .header').length; i++) {
+     let HeaderGetClass    = shortCutAll('#wiki .header')[i].getAttribute('class')
+     let HeaderSliceNum    = HeaderGetClass.slice(HeaderGetClass.length - 1, HeaderGetClass.length)
+     let HeaderTextFilter  = shortCutAll('#wiki .header')[i].innerText.split('(')[0]
+     let HeaderTextDisplay = `<li><span class="list-h${HeaderSliceNum}">${HeaderTextFilter}</span></li>`
+
+     shortCut(`${HeaderMainPath}`).innerHTML += HeaderTextDisplay
+}
+
+let HeadNumIncre = 0
+for (let getHeads = 0; getHeads < shortCutAll('#wiki .header').length; getHeads++) {
+     let HeadIdValue = shortCutAll('#wiki .header')[getHeads].textContent.split('(')[0].trim() + HeadNumIncre++
+     shortCutAll('#wiki .header')[getHeads].setAttribute('id', HeadIdValue)
+     shortCutAll(`${HeaderMainPath} li`)[getHeads].setAttribute('onclick', `window.location.href = '#${HeadIdValue}'`)
+}
+
+function searchInputHeading() {
+     const inputStuff = shortCut('#global-sidebar input');
+     const listStuff  = shortCutAll(`${HeaderMainPath} li`);
+     for (let i = 0; i < listStuff.length; i++) {
+          let spanNestStuff = listStuff[i].getElementsByTagName("span")[0];
+          let spanNestValue = spanNestStuff.textContent.trim() || spanNestStuff.innerText.trim();
+          if (spanNestValue.toUpperCase().indexOf(inputStuff.value.toUpperCase().trim()) >= 0) {
+               listStuff[i].style.display = "";
+          } else {
+               listStuff[i].style.display = "none";
+          }
+     }
+}
+
+shortCut('#sidebar-search input').addEventListener('keyup', searchInputHeading) /* definitely not stolen code */
+shortCut('#sidebar-search #sidebar-clearIcon').addEventListener('click', function() {
+     shortCut('#global-sidebar input').value = ''
+     searchInputHeading()
+})
+
+function sidebarScrollFix() {
+     if (window.scrollY >= 84.5) {
+          shortCut('#global-sidebar').style.position = 'fixed'
+          shortCut('#global-sidebar').style.top      = '0cm'
+     } else {
+          shortCut('#global-sidebar').style.position = 'absolute'
+          shortCut('#global-sidebar').style.top      = '2.25cm'
+     }
+}
+
+sidebarScrollFix()
+window.addEventListener('scroll', sidebarScrollFix)
